0226458b7028490b5db529806ac348994372fcd4
Nandhukriss <103727372+Nandhukriss@users.noreply.github.com>
2023-12-27 19:43:45 +0530

    added footer color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ function Footer() {
   return (
     <div>
 
-    <footer class="shadow  dark:bg-rose-800">
+    <footer class="shadow  bg-rose-800">
         <div class="w-full mx-auto max-w-screen-xl p-6 md:flex md:items-center md:justify-center">
-          <span class="text-sm text-gray-500 sm:text-center dark:text-white ">© 2023 <a href="https://flowbite.com/" class="hover:underline">Image Searcher™</a>. All Rights Reserved.
+          <span class="text-sm sm:text-center text-white ">© 2023 <a href="https://flowbite.com/" class="hover:underline">Image Searcher™</a>. All Rights Reserved.
         </span>
         </div>
     </footer>
